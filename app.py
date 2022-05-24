9053b62965cf39cb5c78aebf0695fba126158f0d
Afreesafatima <afreesafatima133@gmail.com>
2022-05-24 23:23:54 +0530

    make it better

--- app.py
+++ app.py
@@ -40,73 +40,73 @@ if uploaded_file is not None:
                st.header("Total Links")
                st.title(num_links)
           #finding most busiest user
-if selected_user=='Overall':
-     st.title("Most_buzy_user")
-     x,new_df=helper.most_buzy(df)
-     fig,ax=plt.subplots()
+     if selected_user=='Overall':
+       st.title("Most_buzy_user")
+       x,new_df=helper.most_buzy(df)
+       fig,ax=plt.subplots()
 
-     col1,col2=st.columns(2)
-     with col1:
+       col1,col2=st.columns(2)
+       with col1:
           st.header("Bar Chart")
           ax.barh(x.index, x.values,color='red')
           plt.xticks(rotation='vertical')
           st.pyplot(fig)
-     with col2:
+       with col2:
           st.header("Percentage Analysis")
           st.dataframe(new_df)
 
 
-most_common_df=helper.most_common_words(selected_user,df)
-st.title('most_common_words')
-fig,ax=plt.subplots()
-plt.barh(most_common_df[0],most_common_df[1])
-st.pyplot(fig)
-plt.xticks(rotation='vertical')
-#emoji analysis
-x=helper.emoji(selected_user,df,emoji.UNICODE_EMOJI)
-st.title("Emoji Analysis")
-col1,col2=st.columns(2)
-with col1:
-
-  st.dataframe(x)
-with col2:
-  fig,ax=plt.subplots()
-  ax.pie(x[1],labels=x[0],autopct="%.2f")
-  st.pyplot(fig)
-  plt.show()
-  # monthly timeline
-st.title("Monthly Timeline")
-timeline=helper.monthly_timeline(selected_user,df)
-fig,ax=plt.subplots()
-ax.plot(timeline['time'],timeline['messages'],c='green')
-plt.xticks(rotation='vertical')
-st.pyplot(fig)
-#daily timeline
-st.title("Daily Timeline")
-timeline=helper.daily_timeline(selected_user,df)
-fig,ax=plt.subplots()
-ax.plot(timeline['only_date'],timeline['messages'],c='red')
-plt.xticks(rotation='vertical')
-st.pyplot(fig)
-#activity map
-st.title("Activity Map")
-col1,col2=st.columns(2)
-with col1:
-     st.header("Most busy day")
-     busy_day=helper.week_activity_map(selected_user,df)
-     fig, ax = plt.subplots()
-     ax.bar(busy_day.index,busy_day.values)
+     most_common_df=helper.most_common_words(selected_user,df)
+     st.title('most_common_words')
+     fig,ax=plt.subplots()
+     plt.barh(most_common_df[0],most_common_df[1])
      st.pyplot(fig)
-with col2:
-     st.header("Most busy month")
-     busy_day=helper.monthly_activity_map(selected_user,df)
-     fig, ax = plt.subplots()
-     ax.bar(busy_day.index,busy_day.values,color='orange')
+     plt.xticks(rotation='vertical')
+     #emoji analysis
+     x=helper.emoji(selected_user,df,emoji.UNICODE_EMOJI)
+     st.title("Emoji Analysis")
+     col1,col2=st.columns(2)
+     with col1:
+
+        st.dataframe(x)
+     with col2:
+      fig,ax=plt.subplots()
+     ax.pie(x[1],labels=x[0],autopct="%.2f")
+     st.pyplot(fig)
+     plt.show()
+     # monthly timeline
+     st.title("Monthly Timeline")
+     timeline=helper.monthly_timeline(selected_user,df)
+     fig,ax=plt.subplots()
+     ax.plot(timeline['time'],timeline['messages'],c='green')
      plt.xticks(rotation='vertical')
      st.pyplot(fig)
-st.title("Activity Heatmap")
-user_heatmap=helper.activity_heatmap(selected_user,df)
-fig, ax = plt.subplots()
-ax=sns.heatmap(user_heatmap)
-st.pyplot(fig)
+     #daily timeline
+     st.title("Daily Timeline")
+     timeline=helper.daily_timeline(selected_user,df)
+     fig,ax=plt.subplots()
+     ax.plot(timeline['only_date'],timeline['messages'],c='red')
+     plt.xticks(rotation='vertical')
+     st.pyplot(fig)
+     #activity map
+     st.title("Activity Map")
+     col1,col2=st.columns(2)
+     with col1:
+      st.header("Most busy day")
+      busy_day=helper.week_activity_map(selected_user,df)
+      fig, ax = plt.subplots()
+      ax.bar(busy_day.index,busy_day.values)
+      st.pyplot(fig)
+     with col2:
+        st.header("Most busy month")
+        busy_day=helper.monthly_activity_map(selected_user,df)
+        fig, ax = plt.subplots()
+        ax.bar(busy_day.index,busy_day.values,color='orange')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+     st.title("Activity Heatmap")
+     user_heatmap=helper.activity_heatmap(selected_user,df)
+     fig, ax = plt.subplots()
+     ax=sns.heatmap(user_heatmap)
+     st.pyplot(fig)
 
